1ed3c93a87236427c1c153b102a512e9b0e60dec
Moa Torres <moacirtorres@gmail.com>
2021-05-05 16:17:11 -0300

    new: decorators

---
+++ design-patterns/factory-function-05.js
@@ -0,0 +1,45 @@
+class Person {
+  constructor(name = 'unnamed person') {
+    this.name = name
+  }
+
+  toString() {
+    return JSON.stringify(this)
+  }
+}
+
+class Shopper extends Person {
+  constructor(name, money = 0) {
+    super(name)
+    this.money = money
+    this.employed = false
+  }
+}
+
+class Employee extends Shopper {
+  constructor(name, money = 0, employer = '') {
+    super(name, money)
+    this.employer = employer
+    this.employed = true
+  }
+
+  payDay(money = 0) {
+    this.money += money
+  }
+}
+
+const userFactory = (name, money = 0, type, employer) => {
+  if (type === 'employee') {
+    return new Employee(name, money, employer)
+  } else {
+    return new Shopper(name, money)
+  }
+}
+
+const alex = userFactory('Alex Banks', 100)
+const eve = userFactory('Eve Porcello', 100, 'employee', 'This and That')
+
+eve.payDay(100)
+
+console.log(alex.toString())
+console.log(eve.toString())
