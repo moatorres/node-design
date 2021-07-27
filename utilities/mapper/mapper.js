49d37e78df255fe812677f4c6945855355619373
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-26 23:42:27 -0300

    update: reorganizing folders

--- utilities/mapper/mapper.js
+++
@@ -1,8 +0,0 @@
-import map from './map'
-
-function mapper(schemaOrData, schema) {
-  if (!schema) return (value) => map(value, schemaOrData)
-  return map(schemaOrData, schema)
-}
-
-export default mapper
